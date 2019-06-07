164ab0984bc86cd878f9b797db00c5d857a241d7
Tery Lim <kumabotz@gmail.com>
2019-06-07 20:30:48 +1200

    Add pan gesture

--- Assets/Scripts/Gameplay/UserInputHandler.cs
+++ Assets/Scripts/Gameplay/UserInputHandler.cs
@@ -1,20 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UserInputHandler : MonoBehaviour
 {
     public delegate void TapAction(Touch t);
     public static event TapAction OnTap;
 
+    public delegate void PanBeganAction(Touch t);
+    public static event PanBeganAction OnPanBegan;
+
+    public delegate void PanHeldAction(Touch t);
+    public static event PanHeldAction OnPanHeld;
+
+    public delegate void PanEndedAction(Touch t);
+    public static event PanEndedAction OnPanEnded;
+
+    /// <summary>
+    /// Minimum time to hold the finger on the screen to register as a pan gesture
+    /// </summary>
+    public float panMinTime = 0.4f;
+    
+    /// <summary>
+    /// The time when finger is placed on the screen. It's used later to check if it is a pan gesture.
+    /// </summary>
+    private float startTime;
+    private bool panGestureRecognized = false;
+
     public float tapMaxMovement = 50f;
     private Vector2 movement;
     private bool tapGestureFailed;
-
-    void Start()
-    {
-
-    }
 
     void Update()
     {
@@ -24,11 +37,25 @@ public class UserInputHandler : MonoBehaviour
             if (touch.phase == TouchPhase.Began)
             {
                 movement = Vector2.zero;
+                startTime = Time.time;
             }
             else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
             {
                 movement += touch.deltaPosition;
-                if (movement.magnitude > tapMaxMovement)
+
+                if (!panGestureRecognized && Time.time - startTime > panMinTime)
+                {
+                    // pan started
+                    panGestureRecognized = true;
+                    tapGestureFailed = true;
+                    OnPanBegan?.Invoke(touch);
+                }
+                else if (panGestureRecognized)
+                {
+                    // panning
+                    OnPanHeld?.Invoke(touch);
+                }
+                else if (movement.magnitude > tapMaxMovement)
                 {
                     tapGestureFailed = true;
                 }
@@ -36,14 +63,18 @@ public class UserInputHandler : MonoBehaviour
             else
             {
                 // TouchPhase.Ended or TouchPhase.Canceled
-                if (!tapGestureFailed)
+                if (panGestureRecognized)
                 {
-                    if (OnTap != null)
-                    {
-                        OnTap(touch);
-                    }
+                    // pan ended
+                    OnPanEnded?.Invoke(touch);
                 }
-
+                else if (!tapGestureFailed)
+                {
+                    // tap
+                    OnTap?.Invoke(touch);
+                }
+                // reset
+                panGestureRecognized = false;
                 tapGestureFailed = false;
             }
         }
